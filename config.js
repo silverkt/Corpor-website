7515128a1dfc415fc2ecc4b8ae4f947dabc8d5ef
Silver.Sun <itisagift@yeah.net>
2016-12-06 20:56:40 +0800

    upd

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 System.config({
-  baseURL: "/",
+  baseURL: "/XinAo/",
   defaultJSExtensions: true,
   transpiler: "typescript",
   paths: {
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*"
-  },
+  },  
 
   map: {
     "jquery": "npm:jquery@3.1.1",
