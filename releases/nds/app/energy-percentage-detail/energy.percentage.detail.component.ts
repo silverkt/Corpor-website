7029f8f0ad1342e2d7c67acfaf90e194321d89ea
silver.sun <itisagift@yeah.net>
2017-02-16 16:29:28 +0800

    Test the new map idea

--- releases/nds/app/energy-percentage-detail/energy.percentage.detail.component.ts
+++ releases/nds/app/energy-percentage-detail/energy.percentage.detail.component.ts
@@ -4,8 +4,66 @@ import { ActivatedRoute, Params }   from '@angular/router';
 @Component({
     moduleId: module.id,
     selector: "main-board",
-    template: `1234551{{scope}}`,
-    styles: [``]
+    template: `
+<div class="container-fluid">
+    <div class="row" id="all">
+        <div class="col-md-2"><div class="sample1"></div></div>
+        <div class="col-md-2"><div class="sample2"></div></div>
+        <div class="col-md-2"><div class="sample1"></div></div>
+        <div class="col-md-6"><div class="sample2"></div></div>              
+    </div>
+    <div class="row" id="co2">
+        <div class="col-md-2">1</div>
+        <div class="col-md-2"><div class="sample1"></div></div>
+        <div class="col-md-2">1</div>
+        <div class="col-md-6"><div class="sample1"></div></div>              
+    </div>  
+    <div class="row" id="save">
+        <div class="col-md-2"><div class="sample1"></div></div>
+        <div class="col-md-2">1</div>
+        <div class="col-md-2"><div class="sample1"></div></div>
+        <div class="col-md-6">1</div>              
+    </div>  
+    <div class="row" id="clear">
+        <div class="col-md-2">1</div>
+        <div class="col-md-2">1</div>
+        <div class="col-md-2">1</div>
+        <div class="col-md-6">1</div>              
+    </div>  
+</div>
+    `,
+    styles: [`
+     #all>div{
+              height:270px;
+          }
+          
+          #co2>div{
+              height:270px;
+          }
+          
+          #save>div{
+              height:270px;
+          }
+          
+          #clear>div{
+              height:269px;
+              background-color: aquamarine;
+          }
+          
+          .sample1{
+              width: 100%;
+              background-color: black;
+              height: 270px;
+          }
+          .sample2{
+              width: 100%;
+               height: 270px;
+               
+          }
+          
+        
+
+    `]
 })
 export class EnergyPercentageDetailComponent{
     scope: string;
