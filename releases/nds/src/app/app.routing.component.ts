6b72f660f32c20ab06564e0e8f5f6f5170f8b01a
silver.sun <itisagift@yeah.net>
2017-03-21 10:41:58 +0800

    new design of front

---
+++ releases/nds/src/app/app.routing.component.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
+
+const routes: Routes = [
+                            {   
+                                path: '',
+                                component: AppComponent,
+                            },
+                            { 
+                                path: ':id',
+                                component: AppComponent,
+                            }                                         
+                            
+                        ];
+@NgModule({
+  imports: [RouterModule.forRoot(routes,{
+      useHash: true
+  })],
+  exports: [ RouterModule ], 
+})
+export class AppRoutingModule { }
---
+++ releases/nds/src/app/root.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    moduleId: 'approot',
+    selector: 'app-root',
+    template: `<router-outlet>    
+</router-outlet>`,
+
+})
+export class RootComponent {
+    
+}
