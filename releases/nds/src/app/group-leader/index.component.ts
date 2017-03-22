b569722f124e38e34406ef5de84b27333d3291b8
silver.sun <itisagift@yeah.net>
2017-03-22 17:02:14 +0800

    main layout on going

--- releases/nds/src/app/group-leader/index.component.ts
+++ releases/nds/src/app/group-leader/index.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var BMap: any;
+ 
 
 @Component({
     moduleId: 'big-client-index',
@@ -6,6 +8,13 @@ import { Component } from '@angular/core';
     templateUrl: 'index.component.html',
     styleUrls: ['index.component.css'],
 })
-export class GroupIndexComponent {
-    
+export class GroupIndexComponent implements OnInit {
+    ngOnInit() {
+        // 百度地图API功能
+	var map = new BMap.Map("allmap");    // 创建Map实例
+	map.centerAndZoom(new BMap.Point(116.404, 36.915), 6);  // 初始化地图,设置中心点坐标和地图级别
+	map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
+	map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
+	map.enableScrollWheelZoom(false);     //开启鼠标滚轮缩放
+    }
 }
