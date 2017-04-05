d845d1520b07f453c746b7102075f8272e2fc52a
silver.sun <itisagift@yeah.net>
2017-04-05 16:32:38 +0800

    Working on National energy benifit 1. 完成集团领导区域地图首页全国泛能收益状况展现内容布局，样式等 2. 完成集团领导区域地图首页全国泛能收益状况各多能源泛能收益对比饼图组件样式

--- releases/nds/src/app/group-leader/index.component.ts
+++ releases/nds/src/app/group-leader/index.component.ts
@@ -25,7 +25,43 @@ export class GroupIndexComponent implements OnInit {
 	map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
 	map.enableScrollWheelZoom(false);     //开启鼠标滚轮缩放   
 
+
+
+
+     var myChart = echarts.init(document.getElementById('tt1'));         
+             var option = {
+                    title: {
+                text: '多能源成交占比',
+                x: 'center',
+                textStyle: {
+                    fontWeight: 'normal',
+                    fontSize: '12',
+                }
+            },
+    color: ['#9bbb59','#b3a2c7','#00b0f0', '#8eb4e3'],
+    series: [{
+        name: '多能源成交占比',
+        type: 'pie',
+        radius: [0,'60%'],
+        label: {
+            normal: {
+                formatter: '{b}{d}%',
+            }
+        },
+        labelLine: {
+            normal: {
+                length: 5,
+                length2: 5,
+            }
+        },
+        data: [{name:'其他',value:10},{name:'燃气',value:10},{name:'电力',value:60},{name:'冷/热',value:20}]
+    }]
+             }
+            myChart.setOption(option);    
+
     }
+
+    
 
     changeScope() {
         this.clickedScope.name = "山东";
