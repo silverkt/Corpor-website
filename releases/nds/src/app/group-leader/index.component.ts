b29239b7596638610555abc36530aee827dc37b3
silver.sun <itisagift@yeah.net>
2017-04-06 16:30:39 +0800

    Add a National Energy benifit line style 1. 完成集团领导区域地图首页全国泛能收益状况泛能收益折线图组件外观样式

--- releases/nds/src/app/group-leader/index.component.ts
+++ releases/nds/src/app/group-leader/index.component.ts
@@ -31,7 +31,7 @@ export class GroupIndexComponent implements OnInit {
      var myChart = echarts.init(document.getElementById('tt1'));         
              var option = {
                     title: {
-                text: '多能源成交占比',
+                text: '各多能源泛能收益对比',
                 x: 'center',
                 textStyle: {
                     fontWeight: 'normal',
@@ -57,11 +57,73 @@ export class GroupIndexComponent implements OnInit {
         data: [{name:'其他',value:10},{name:'燃气',value:10},{name:'电力',value:60},{name:'冷/热',value:20}]
     }]
              }
-            myChart.setOption(option);    
+            myChart.setOption(option);  
+
+
+  var myChart = echarts.init(document.getElementById('tt2'));         
+             var option2 = {
+                   
+
+legend: {
+            data: ['泛能收益'],
+            padding: [30,0,0,0]
+        },
+
+        grid: {
+            show: true,
+            containLabel: true
+        },
+        xAxis: {
+            type: 'category',
+            data: ['2/4','2/6','2/8','2/10','2/12','2/14'],
+            axisLabel: {
+                interval: 0,
+                textStyle: {
+                    fontSize: 9 
+                }
+            },
+            axisTick: {
+                alignWithLabel: true,
+            }
+        },
+        yAxis: [{
+            name: '泛能收益/元',
+            nameLocation: 'middle',
+            nameGap: 20,
+            type: 'value',
+            axisLabel: {
+                textStyle: {
+                    fontSize: 9 
+                }
+            },
+        }],
+        series: [
+            {
+                name:'泛能收益',
+                type:'line',
+                data:[3, 6, 8, 9, 10, 8],
+                itemStyle: {
+                    normal: {
+                        color: '#00b0f0',
+                    }
+                }
+            } 
+        ]
+
+
+
+
+             }
+            myChart.setOption(option2);  
+
+
+
+
+
 
     }
 
-    
+
 
     changeScope() {
         this.clickedScope.name = "山东";
