2614db9ffb7a6a2df58d2fbcc1434b0783e43a6d
silver.sun <itisagift@yeah.net>
2017-04-11 16:47:19 +0800

    Layout of Profit Ranking 1. 完成集团领导区域地图首页全国泛能收益排行榜展现内容布局，样式等

--- releases/nds/src/app/group-leader/index.component.ts
+++ releases/nds/src/app/group-leader/index.component.ts
@@ -24,7 +24,43 @@ export class GroupIndexComponent implements OnInit {
 	map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
 	map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
 	map.enableScrollWheelZoom(false);     //开启鼠标滚轮缩放   
+////////////////////////////////////
 
+var myChart = echarts.init(document.getElementById('tt1'));         
+var option = {
+    title: {
+                text: ' ',
+                x: 'center',
+                textStyle: {
+                    fontWeight: 'normal',
+                    fontSize: '12',
+                }
+            },
+    color: ['#9bbb59','#b3a2c7','#00b0f0', '#8eb4e3'],
+    series: [{
+        name: '多能源成交占比',
+        type: 'pie',
+        radius: [0,'60%'],
+        label: {
+            normal: {
+                formatter: '{b}{d}%',
+            }
+        },
+        labelLine: {
+            normal: {
+                length: 5,
+                length2: 5,
+            }
+        },
+        data: [{name:'其他',value:10},{name:'华东',value:10},{name:'华北',value:60},{name:'山东',value:20}]
+    }]
+
+}
+myChart.setOption(option);        
+
+
+
+////////////////////////////////////
     }
 
 
